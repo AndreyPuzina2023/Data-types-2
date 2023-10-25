ccc11803e10fe2bdf42edf2f04489a753c461f59
AndreyP <puzinaandrey2023@gmail.com>
2023-10-25 14:54:24 +0300

    izmenen

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@
 public class Main {
     public static void main(String[] args) {
         int check = 100; //изначально на счете
-        int topUpAmount = 2000; // сумма пополнения
+        int topUpAmount = 900; // сумма пополнения
+        int bonusAmount = 0; // Изначальная сумма бонуса
         System.out.println("Сумма баланса до пополнения =" + check);
         System.out.println("Сумма пополнеия составляет = " + topUpAmount);
         int balans = check + topUpAmount;
@@ -11,8 +12,10 @@ public class Main {
         if (topUpAmount > 1000) {
             int bonus = topUpAmount / 100;
             int inputbonus = bonus + balans;
+            System.out.println("Начисленные бонусы "+bonus);
             System.out.println("Сумма с учетом бонусов за пополнение =" + inputbonus);
-        } else System.out.println("Пополните сумму более чем на 1000 р для получения бонуса!");
+        } else System.out.println("Сумма бонусов ="+ bonusAmount);
+
     }
 
 }
